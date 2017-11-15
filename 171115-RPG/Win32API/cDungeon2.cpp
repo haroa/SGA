0c5a67259f9c486a91d0a246f4f3042ef4bdc2f5
haroa <32758491+haroa@users.noreply.github.com>
2017-11-15 16:18:41 +0900

    끝!

--- 171115-RPG/Win32API/cDungeon2.cpp
+++ 171115-RPG/Win32API/cDungeon2.cpp
@@ -5,22 +5,94 @@
 void cDungeon2::Setup()
 {
 	m_cUi.Setup();
+	m_cPlayer.Setup();
+	JsonLoad();
+	m_fMonster1X = 200;
+	m_fMonster1Y = 200;
+	m_rtMonster1 = RectMake(m_fMonster1X, m_fMonster1Y, 60, 60);
+
+	m_fMonster2X = 600;
+	m_fMonster2Y = 400;
+	m_rtMonster2 = RectMake(m_fMonster2X, m_fMonster2Y, 60, 60);
+
+	m_fMonster3X = 500;
+	m_fMonster3Y = 500;
+	m_rtMonster3 = RectMake(m_fMonster3X, m_fMonster3Y, 60, 60);
 }
 
 void cDungeon2::Update()
 {
 	if (g_pKeyManager->isOnceKeyDown(VK_ESCAPE))
 	{
+		JsonSave();
 		g_pSceneManager->ChangeScene(SLIST_PLAY);
+		m_cPlayer.SetPosX(WINSIZEX * 0.5f);
+		m_cPlayer.SetPosY(WINSIZEY * 0.5f);
 	}
+	m_cPlayer.Update();
+	Collision();
 }
 
 void cDungeon2::Render()
 {
-	TextOut(g_hDC, 100, 100, "带傈2", strlen("带傈2"));
+	//TextOut(g_hDC, 100, 100, "带傈1", strlen("带傈1"));
 	m_cUi.Render();
+	m_cPlayer.Render();
+	EllipseMake(g_hDC, m_rtMonster1);
+	EllipseMake(g_hDC, m_rtMonster2);
+	EllipseMake(g_hDC, m_rtMonster3);
 }
 
 void cDungeon2::Release()
 {
 }
+
+void cDungeon2::Collision()
+{
+	RECT rt1;
+	if (IntersectRect(&rt1, &m_cPlayer.GetBody(), &m_rtMonster1))
+	{
+		m_cUi.SetHp(m_cUi.GetHp() - 10);
+		m_cUi.SetMoney(m_cUi.GetMoney() + 500);
+		m_rtMonster1 = RectMake(0, 0, 0, 0);
+	}
+	else if (IntersectRect(&rt1, &m_cPlayer.GetBody(), &m_rtMonster2))
+	{
+		m_cUi.SetHp(m_cUi.GetHp() - 10);
+		m_cUi.SetMoney(m_cUi.GetMoney() + 500);
+		m_rtMonster2 = RectMake(0, 0, 0, 0);
+	}
+	else if (IntersectRect(&rt1, &m_cPlayer.GetBody(), &m_rtMonster3))
+	{
+		m_cUi.SetHp(m_cUi.GetHp() - 10);
+		m_cUi.SetMoney(m_cUi.GetMoney() + 500);
+		m_rtMonster3 = RectMake(0, 0, 0, 0);
+	}
+}
+
+void cDungeon2::JsonSave()
+{
+	json savedata;
+	savedata["Player"]["money"] = m_cUi.GetMoney();
+	savedata["Player"]["potion1"] = m_cUi.GetPotion1();
+	savedata["Player"]["potion2"] = m_cUi.GetPotion2();
+	savedata["Player"]["atk"] = m_cUi.GetAtk();
+	savedata["Player"]["def"] = m_cUi.GetDef();
+	savedata["Player"]["hp"] = m_cUi.GetHp();
+	g_pFileManager->JsonSave(JSON_PATH, savedata.dump());
+}
+
+void cDungeon2::JsonLoad()
+{
+	json loaddata;
+	g_pFileManager->JsonLoad("savedata", JSON_PATH);
+	loaddata = g_pFileManager->JsonFind("savedata");
+	m_cUi.SetMoney(loaddata["Player"]["money"]);
+	m_cUi.SetPotion1(loaddata["Player"]["potion1"]);
+	m_cUi.SetPotion2(loaddata["Player"]["potion2"]);
+	m_cUi.SetAtk(loaddata["Player"]["atk"]);
+	m_cUi.SetDef(loaddata["Player"]["def"]);
+	m_cUi.SetHp(loaddata["Player"]["hp"]);
+	g_pFileManager->JsonRelease("savedata");
+}
+
--- 171115-RPG/Win32API/cDungeon2.h
+++ 171115-RPG/Win32API/cDungeon2.h
@@ -1,10 +1,29 @@
 #pragma once
 #include "cGameNode.h"
 #include "cUi.h"
+#include "cPlayer.h"
 class cDungeon2 : public cGameNode
 {
 private:
 	cUi m_cUi;
+
+	cPlayer	m_cPlayer;
+
+	float	 m_fMonster1X;
+	float	 m_fMonster1Y;
+	RECT	 m_rtMonster1;
+
+	float	 m_fMonster2X;
+	float	 m_fMonster2Y;
+	RECT	 m_rtMonster2;
+
+	float	 m_fMonster3X;
+	float	 m_fMonster3Y;
+	RECT	 m_rtMonster3;
+private:
+	void Collision();
+	void JsonSave();
+	void JsonLoad();
 public:
 	cDungeon2() {};
 	virtual ~cDungeon2() {};
