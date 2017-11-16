1cb730f9146330d5c49cc74b20a3d1d07e35096a
haroa <32758491+haroa@users.noreply.github.com>
2017-11-16 23:08:46 +0900

    개정판

--- 171115-RPG/Win32API/cUi.cpp
+++ 171115-RPG/Win32API/cUi.cpp
@@ -19,36 +19,36 @@ void cUi::Setup()
 #pragma region Information
 	m_finformationX = 800;
 	m_finformationY = 0;
-	m_rtinformation = RectMake(m_finformationX, m_finformationY, 100, 50);
+	m_rtinformation = RectMake(m_finformationX, m_finformationY, 200, 50);
 
 	m_fPotion1X = 800;
 	m_fPotion1Y = 50;
-	m_rtPotion1 = RectMake(m_fPotion1X, m_fPotion1Y, 100, 50);
+	m_rtPotion1 = RectMake(m_fPotion1X, m_fPotion1Y, 200, 50);
 	m_nCountPotion1 = 10;
 
 	m_fPotion2X = 800;
 	m_fPotion2Y = 100;
-	m_rtPotion2 = RectMake(m_fPotion2X, m_fPotion2Y, 100, 50);
+	m_rtPotion2 = RectMake(m_fPotion2X, m_fPotion2Y, 200, 50);
 	m_nCountPotion2 = 10;
 
 	m_fHpX = 800;
 	m_fHpY = 150;
-	m_rtHp = RectMake(m_fHpX, m_fHpY, 100, 50);
+	m_rtHp = RectMake(m_fHpX, m_fHpY, 200, 50);
 	m_nHp = 100;
 
 	m_fAtkX = 800;
 	m_fAtkY = 200;
-	m_rtAtk = RectMake(m_fAtkX, m_fAtkY, 100, 50);
+	m_rtAtk = RectMake(m_fAtkX, m_fAtkY, 200, 50);
 	m_nAtk = 30;
 
 	m_fDefX = 800;
 	m_fDefY = 250;
-	m_rtDef = RectMake(m_fDefX, m_fDefY, 100, 50);
+	m_rtDef = RectMake(m_fDefX, m_fDefY, 200, 50);
 	m_nDef = 20;
 
 	m_fMoneyX = 800;
 	m_fMoneyY = 300;
-	m_rtMoney = RectMake(m_fMoneyX, m_fMoneyY, 100, 50);
+	m_rtMoney = RectMake(m_fMoneyX, m_fMoneyY, 200, 50);
 	m_nMoney = 500000;
 #pragma endregion
 
@@ -61,7 +61,11 @@ void cUi::Update()
 void cUi::Render()
 {
 	Rectangle(g_hDC, m_rtInfo.left, m_rtInfo.top, m_rtInfo.right, m_rtInfo.bottom);
-	TextOut(g_hDC, m_finformationX, m_finformationY, "==========정보===========", strlen("==========정보==========="));
+	//TextOut(g_hDC, m_finformationX, m_finformationY, "==========정보===========", strlen("==========정보==========="));
+
+	string szinfo = "==========정보===========";
+	DrawTextBox(g_hDC, m_rtinformation, 15, szinfo);
+	
 	string szpotion1 = "포션1 : ";
 	szpotion1.append(to_string(m_nCountPotion1));
 	DrawTextBox(g_hDC, m_rtPotion1, 15, szpotion1);
